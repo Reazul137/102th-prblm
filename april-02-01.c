e33846493c623e9b26dd95cf86c5e85dadb36352
Reazul Islam <reazulislam137@gmail.com>
2018-04-02 00:17:05 +0600

    first commit

---
+++ april-02-01.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int sidea, sideb, sidec;
+
+    printf("Input three side of triangle : ");
+    scanf("%d %d %d",&sidea, &sideb, &sidec);
+
+    if(sidea == sideb && sideb == sidec)
+    {
+        printf("This is an equilateral triangle.\n");
+    }
+    else if (sidea == sideb || sidea ==  sidec || sideb == sidec)
+    {
+        printf("This is an isosceles triangle.\n");
+    }
+    else
+    {
+        printf("This is an scalene triangle.");
+    }
+
+    return 0;
+}
